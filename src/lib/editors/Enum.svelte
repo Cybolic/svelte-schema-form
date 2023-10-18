a479a77f810ae93864bead94e37d26a5239e6012
Christian Dannie Storgaard <cds@cybolic.me>
2023-10-18 15:22:55 +0300

    Added `enumDefault` schema parameter

--- src/lib/editors/Enum.svelte
+++ src/lib/editors/Enum.svelte
@@ -4,22 +4,29 @@
 	export let schema: any;
 	export let value: any;
 	let enumVals: string[];
-	let enumText: string[]
+	let enumText: string[];
 	$: enumVals = schema.enum;
 	$: enumText = schema.enumText || schema.enum;
+	$: enumDefault = schema.defaultValue;
+	$: if (value == undefined && enumDefault != undefined) {
+		value = enumDefault;
+		params.pathChanged(params.path, value);
+	};
 </script>
 
 <!-- event which calls pathChanged should be after all bindings so 'value' will have been updated -->
 <svelte:component this={params.components['fieldWrapper']} {params} {schema}>
 	<select id={params.path.join('.')}
 		name={params.path.join('.')}
-		value={value} 
+		value={value}
 		disabled={schema.readOnly || params.containerReadOnly}
 		data-hidden={schema.hidden}
-		on:change={ev => params.pathChanged(params.path, ev.currentTarget.value || undefined)}>
+		on:change={ev => params.pathChanged(params.path, value || ev.currentTarget.value || undefined)}>
 		<option></option>
 		{#each enumVals as enumVal, idx}
-		<option value={enumVal}>{(enumText || [])[idx]}</option>
+		<option
+			value={enumVal}
+		>{(enumText || [])[idx]}</option>
 		{/each}
 	</select>
 </svelte:component>
